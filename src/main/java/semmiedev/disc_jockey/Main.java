e6f7634d9389bfad60081a878abd2286c3273fb1
Semmieboy YT <semvdmeij124@gmail.com>
2022-05-31 18:11:50 +0200

    Added key bind for opening song selection screen

--- src/main/java/semmiedev/disc_jockey/Main.java
+++ src/main/java/semmiedev/disc_jockey/Main.java
@@ -1,17 +1,23 @@
 package semmiedev.disc_jockey;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.networking.v1.ClientLoginConnectionEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.lwjgl.glfw.GLFW;
 import semmiedev.disc_jockey.gui.hud.BlocksOverlay;
+import semmiedev.disc_jockey.gui.screen.DiscJockeyScreen;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -36,6 +42,8 @@ public class Main implements ClientModInitializer {
 
         SongLoader.loadSongs();
 
+        KeyBinding openScreenKeyBind = KeyBindingHelper.registerKeyBinding(new KeyBinding(MOD_ID+".key_bind.open_screen", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_J, "key.category."+MOD_ID));
+
         ClientTickEvents.START_CLIENT_TICK.register(new ClientTickEvents.StartTick() {
             private ClientWorld prevWorld;
 
@@ -46,6 +54,14 @@ public class Main implements ClientModInitializer {
                     SONG_PLAYER.stop();
                 }
                 prevWorld = client.world;
+
+                if (openScreenKeyBind.wasPressed()) {
+                    if (SongLoader.loadingSongs) {
+                        client.inGameHud.getChatHud().addMessage(Text.translatable(Main.MOD_ID+".still_loading"));
+                    } else {
+                        client.setScreen(new DiscJockeyScreen());
+                    }
+                }
             }
         });
         ClientTickEvents.START_WORLD_TICK.register(world -> {
